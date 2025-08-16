4cac1bc07df8a38fa0a19a3f99444336fd59d0c8
Krishna9879 <krishna.paridwal.cg@gmail.com>
2025-08-16 16:26:25 +0530

    added shop option

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
                 }`}
                 onClick={handleNavigation}
               >
-                New Arrivals
+                Shop
                 <svg className="ml-1 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
                 </svg>
